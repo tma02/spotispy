6457e08840792430d24391cdba9357fab7b5026e
Tianyi Ma <tn.ma@outlook.com>
2016-03-13 23:12:02 -0500

    Convert to electron app

--- public/index.js
+++
@@ -1,7 +0,0 @@
-setInterval(function() {
-  xhr = new XMLHttpRequest();
-  xhr.open('GET', '/coverurl', false);
-  xhr.send();
-  document.getElementById('background').src = xhr.responseText;
-  document.getElementById('cover').src = xhr.responseText;
-}, 500);
